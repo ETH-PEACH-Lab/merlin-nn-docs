1a58f8e12f040a923bfeac365b13af4850eace17
sgafari <sgafari@ethz.ch>
2026-06-19 10:45:56 +0200

    Update baseUrl and projectName in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://eth-peach-lab.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/merlin-docs/',
+  baseUrl: '/merlin-nn-doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ETH-PEACH-Lab', // Usually your GitHub org/user name.
-  projectName: 'merlin-docs', // Usually your repo name.
+  projectName: 'merlin-nn-doc', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
