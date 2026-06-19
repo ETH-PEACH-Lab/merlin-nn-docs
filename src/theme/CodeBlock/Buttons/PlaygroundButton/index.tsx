2f98438596b978684388d8aa9623cc520a7ab249
Siawash <sgafari@ethz.ch>
2026-06-19 11:34:22 +0200

    changed name and link

--- src/theme/CodeBlock/Buttons/PlaygroundButton/index.tsx
+++ src/theme/CodeBlock/Buttons/PlaygroundButton/index.tsx
@@ -35,7 +35,7 @@ const PlaygroundButton: React.FC = () => {
     const compressedCode = LZString.compressToBase64(code);
     
     // Create the playground URL
-    const playgroundUrl = `https://eth-peach-lab.github.io/merlin/#/url/${language}/${compressedCode}`;
+    const playgroundUrl = `https://eth-peach-lab.github.io/merlin-nn-editor/#/url/${language}/${compressedCode}`;
     
     // Open in new tab
     window.open(playgroundUrl, '_blank');
@@ -50,13 +50,13 @@ const PlaygroundButton: React.FC = () => {
     <button
       className={styles.playgroundButton}
       onClick={handleOpenInPlayground}
-      title="Open in Merlin Editor"
-      aria-label="Open code in Merlin Editor"
+      title="Open in Merlin NN Editor"
+      aria-label="Open code in Merlin NN Editor"
     >
       <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16">
         <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zM5.904 10.803 10 6.707v2.768a.5.5 0 0 0 1 0V5.5a.5.5 0 0 0-.5-.5H6.525a.5.5 0 1 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 .707.707"/>
       </svg>
-      Open in Merlin Editor
+      Open in Merlin NN Editor
     </button>
   );
 };
